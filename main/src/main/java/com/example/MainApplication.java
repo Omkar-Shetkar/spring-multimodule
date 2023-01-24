eded9a39d040f0e21e5e28e178fde9640e080145
omkar.shetkar <omkar.shetkar@hcl.com>
2023-01-24 16:25:26 +0530

    refactor

--- main/src/main/java/com/example/MainApplication.java
+++ main/src/main/java/com/example/MainApplication.java
@@ -1,17 +1,15 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.module1", "com.example"})
-public class Application {
+public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 
 }
