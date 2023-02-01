0d2148cca3141b7f3df52c5a562b01c17c11202b
omkar.shetkar <omkar.shetkar@hcl.com>
2023-02-01 20:13:58 +0530

    Multimodules with single database.

--- main/src/main/java/com/example/MainApplication.java
+++ main/src/main/java/com/example/MainApplication.java
@@ -2,10 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.module1", "com.example"})
+@EnableJpaRepositories({"com.module1", "com.example"})
+@EntityScan({"com.module1", "com.example"})
 public class MainApplication {
 
     public static void main(String[] args) {
--- main/src/main/java/com/example/api/PersistenceAppAutoConfiguration.java
+++ main/src/main/java/com/example/api/PersistenceAppAutoConfiguration.java
@@ -1,34 +1,15 @@
 package com.example.api;
 
 import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
-@PropertySource({"classpath:application.yml"})
-@EnableJpaRepositories(
-        basePackages = "com.example.api",
-        entityManagerFactoryRef = "appEntityManager",
-        transactionManagerRef = "appTransactionManager")
 public class PersistenceAppAutoConfiguration {
-
-    @Autowired
-    private Environment env;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
@@ -36,39 +17,6 @@ public class PersistenceAppAutoConfiguration {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean
-    @Primary
-    public LocalContainerEntityManagerFactoryBean appEntityManager() {
-        LocalContainerEntityManagerFactoryBean em
-                = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(appDatasource());
-        em.setPackagesToScan(
-                new String[]{"com.example.api"});
-
-        HibernateJpaVendorAdapter vendorAdapter
-                = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto",
-                env.getProperty("spring.jpa.hibernate.ddl-auto"));
-        properties.put("hibernate.dialect",
-                env.getProperty("spring.jpa.hibernate.dialect"));
-        em.setJpaPropertyMap(properties);
-
-        return em;
-    }
-
-
-    @Bean
-    @Primary
-    public PlatformTransactionManager appTransactionManager() {
-
-        JpaTransactionManager transactionManager
-                = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(
-                appEntityManager().getObject());
-        return transactionManager;
-    }
 
     @Bean
     public SpringLiquibase mainLiquibase() {
